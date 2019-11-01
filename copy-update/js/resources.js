4d8c8f00aed58fc0596582b0cbc0a0dffc9bed0a
K <kiyunkim@users.noreply.github.com>
2019-11-01 16:23:34 -0400

    resources.reconstructTable draft

--- copy-update/js/resources.js
+++ copy-update/js/resources.js
@@ -23,6 +23,35 @@ SG.resources = {
     }
     
     this.specialMultiplier = 1;
+    this.reconstructTable();
+  },
+
+  reconstructTable: function() {
+    var $table;
+    var tableId = 'resourceTable';
+    var $status = document.getElementById('status');
+
+    // if table doesn't exist, create one
+    if (document.getElementById(tableId) === null) {
+      // set up title
+      $status.innerHTML = '<h3>stuff</h3>';
+
+      var $tableContainer = document.createElement('div');
+      $tableContainer.setAttribute('id', 'resourceTableContainer');
+
+      $table = document.createElement('div');
+      $table.setAttribute('id', tableId);
+
+      $tableContainer.appendChild($table);
+    }
+
+    // empty table
+    $table.innerHTML = '';
+    var anyResourcesInTable = false;
+  },
+
+  updateTable: function() {
+    
   },
 
   isCategory: function(name) {
